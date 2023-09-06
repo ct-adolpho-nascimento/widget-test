30c599206bcd75291c2b2f41aee4cf5979431ee5
Adolpho Cavalcanti <adolpho.nascimento@climatempo.com.br>
2023-09-06 20:13:39 -0300

    refact: refact using class

--- script-interation.js
+++
@@ -1,6 +0,0 @@
-export function setupBtn() {
-  const btns = document.querySelectorAll('button');
-  btns.forEach(b => b.addEventListener('click', () => {
-    document.documentElement.style.setProperty('--hue', b.dataset.color);
-  }))
-}
